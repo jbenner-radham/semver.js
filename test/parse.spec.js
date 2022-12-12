78811fbf521bf1d24c14c61ec3bbc2c4c60afe63
James Benner <james.benner@gmail.com>
2022-12-12 18:12:02 -0500

    Add more parse tests

--- test/parse.spec.js
+++ test/parse.spec.js
@@ -1,87 +1,132 @@
+import { describe, expect, it } from '@jest/globals';
 import { parse } from '../lib/index.js';
 
-describe('semver', () => {
-    describe('parse', () => {
-        it('is a function', () => {
-            expect(typeof parse).toEqual('function');
+describe('parse', () => {
+    it('is a function', () => {
+        expect(parse).toBeFunction();
+    });
+
+    it('returns an object', () => {
+        expect(parse()).toBeObject();
+    });
+
+    describe('when passed v1.0.0', () => {
+        const VERSION = 'v1.0.0';
+
+        it('returns the major version property', () => {
+            expect(parse(VERSION).major).toEqual(1);
         });
 
-        it('returns an object', () => {
-            expect(typeof parse()).toEqual('object');
+        it('returns the minor version property', () => {
+            expect(parse(VERSION).minor).toEqual(0);
         });
 
-        describe('when passed v1.0.0', () => {
-            it('returns the major version property', () => {
-                expect(parse('v1.0.0').major).toEqual(1);
-            });
-
-            it('returns the minor version property', () => {
-                expect(parse('v1.0.0').minor).toEqual(0);
-            });
-
-            it('returns the patch version property', () => {
-                expect(parse('v1.0.0').patch).toEqual(0);
-            });
+        it('returns the patch version property', () => {
+            expect(parse(VERSION).patch).toEqual(0);
         });
 
-        describe('when passed 0.3.2', () => {
-            it('returns the major version property', () => {
-                expect(parse('0.3.2').major).toEqual(0);
-            });
-
-            it('returns the minor version property', () => {
-                expect(parse('0.3.2').minor).toEqual(3);
-            });
-
-            it('returns the patch version property', () => {
-                expect(parse('0.3.2').patch).toEqual(2);
-            });
+        it('returns an empty preRelease property', () => {
+            expect(parse(VERSION).preRelease).toEqual('');
         });
 
-        describe('when passed v1.5.2-beta.2+fe523', () => {
-            it('returns the major version property', () => {
-                expect(parse('v1.5.2-beta.2+fe523').major).toEqual(1);
-            });
+        it('returns an empty build property', () => {
+            expect(parse(VERSION).build).toEqual('');
+        });
+    });
 
-            it('returns the minor version property', () => {
-                expect(parse('v1.5.2-beta.2+fe523').minor).toEqual(5);
-            });
+    describe('when passed 0.3.2', () => {
+        const VERSION = '0.3.2';
 
-            it('returns the patch version property', () => {
-                expect(parse('v1.5.2-beta.2+fe523').patch).toEqual(2);
-            });
-
-            it('returns the preRelease property', () => {
-                expect(parse('v1.5.2-beta.2+fe523').preRelease).toEqual('beta.2');
-            });
-
-            it('returns the build property', () => {
-                expect(parse('v1.5.2-beta.2+fe523').build).toEqual('fe523');
-            });
+        it('returns the major version property', () => {
+            expect(parse(VERSION).major).toEqual(0);
         });
 
-        describe('when passed 5.0.1+exp.sha.5114f85', () => {
-            const VERSION = '5.0.1+exp.sha.5114f85';
+        it('returns the minor version property', () => {
+            expect(parse(VERSION).minor).toEqual(3);
+        });
 
-            it('returns the major version property', () => {
-                expect(parse(VERSION).major).toEqual(5);
-            });
+        it('returns the patch version property', () => {
+            expect(parse(VERSION).patch).toEqual(2);
+        });
 
-            it('returns the minor version property', () => {
-                expect(parse(VERSION).minor).toEqual(0);
-            });
+        it('returns an empty preRelease property', () => {
+            expect(parse(VERSION).preRelease).toEqual('');
+        });
 
-            it('returns the patch version property', () => {
-                expect(parse(VERSION).patch).toEqual(1);
-            });
+        it('returns an empty build property', () => {
+            expect(parse(VERSION).build).toEqual('');
+        });
+    });
 
-            it('returns an empty preRelease property', () => {
-                expect(parse(VERSION).preRelease).toEqual('');
-            });
+    describe('when passed 1.0.5-rc.1', () => {
+        const VERSION = '1.0.5-rc.1';
 
-            it('returns the build property', () => {
-                expect(parse(VERSION).build).toEqual('exp.sha.5114f85');
-            });
+        it('returns the major version property', () => {
+            expect(parse(VERSION).major).toEqual(1);
+        });
+
+        it('returns the minor version property', () => {
+            expect(parse(VERSION).minor).toEqual(0);
+        });
+
+        it('returns the patch version property', () => {
+            expect(parse(VERSION).patch).toEqual(5);
+        });
+
+        it('returns the preRelease property', () => {
+            expect(parse(VERSION).preRelease).toEqual('rc.1');
+        });
+
+        it('returns an empty build property', () => {
+            expect(parse(VERSION).build).toEqual('');
+        });
+    });
+
+    describe('when passed v1.5.2-beta.2+fe523', () => {
+        const VERSION = 'v1.5.2-beta.2+fe523';
+
+        it('returns the major version property', () => {
+            expect(parse(VERSION).major).toEqual(1);
+        });
+
+        it('returns the minor version property', () => {
+            expect(parse(VERSION).minor).toEqual(5);
+        });
+
+        it('returns the patch version property', () => {
+            expect(parse(VERSION).patch).toEqual(2);
+        });
+
+        it('returns the preRelease property', () => {
+            expect(parse(VERSION).preRelease).toEqual('beta.2');
+        });
+
+        it('returns the build property', () => {
+            expect(parse(VERSION).build).toEqual('fe523');
+        });
+    });
+
+    describe('when passed 5.0.1+exp.sha.5114f85', () => {
+        const VERSION = '5.0.1+exp.sha.5114f85';
+
+        it('returns the major version property', () => {
+            expect(parse(VERSION).major).toEqual(5);
+        });
+
+        it('returns the minor version property', () => {
+            expect(parse(VERSION).minor).toEqual(0);
+        });
+
+        it('returns the patch version property', () => {
+            expect(parse(VERSION).patch).toEqual(1);
+        });
+
+        it('returns an empty preRelease property', () => {
+            expect(parse(VERSION).preRelease).toEqual('');
+        });
+
+        it('returns the build property', () => {
+            expect(parse(VERSION).build).toEqual('exp.sha.5114f85');
         });
     });
 });
