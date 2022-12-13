c774d242999ce89766bac824ce45a755b542db65
James Benner <james.benner@gmail.com>
2022-12-13 15:20:38 -0500

    Add is#greaterThanOrEqualTo function

--- test/is.spec.js
+++ test/is.spec.js
@@ -42,7 +42,7 @@ describe('is', () => {
         });
 
         describe('when passed 1.0.0 to both functions', () => {
-            it('returns true', () => {
+            it('returns false', () => {
                 expect(is('1.0.0').greaterThan('1.0.0')).toBeFalse();
             });
         });
@@ -50,6 +50,34 @@ describe('is', () => {
         describe('when passed 1.0.0 to the first function and 0.1.0 to the second', () => {
             it('returns true', () => {
                 expect(is('1.0.0').greaterThan('0.1.0')).toBeTrue();
+            });
+        });
+
+        describe('when passed 0.5.1 to the first function and 0.5.2 to the second', () => {
+            it('returns false', () => {
+                expect(is('0.5.1').greaterThan('0.5.2')).toBeFalse();
+            });
+        });
+    });
+
+    describe('#greaterThanOrEqualTo', () => {
+        it('is a function', () => {
+            expect(is('1.0.0').greaterThanOrEqualTo).toBeFunction();
+        });
+
+        it('returns a boolean', () => {
+            expect(is('1.0.0').greaterThanOrEqualTo('1.0.0')).toBeBoolean();
+        });
+
+        describe('when passed 1.0.0 to both functions', () => {
+            it('returns true', () => {
+                expect(is('1.0.0').greaterThanOrEqualTo('1.0.0')).toBeTrue();
+            });
+        });
+
+        describe('when passed 1.0.0 to the first function and 0.1.0 to the second', () => {
+            it('returns true', () => {
+                expect(is('1.0.0').greaterThanOrEqualTo('0.1.0')).toBeTrue();
             });
         });
 
