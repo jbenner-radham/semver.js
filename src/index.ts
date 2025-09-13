e423d7c7457b2dd02ba38a114ac70bbdf2f579a6
James Benner <hello@jamesbenner.com>
2025-09-13 17:20:52 -0400

    Fix SemanticVersion export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { SPEC_VERSION } from './constants.js';
 export { default as is } from './is.js';
-export { default as parse, type SemanticVersion } from './parse.js';
+export { default as parse } from './parse.js';
+export { default as SemanticVersion } from './semantic-version';
