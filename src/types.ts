070ac5bcad7d7a13d6a0dc45cacce47dbf9ea28e
James Benner <hello@jamesbenner.com>
2025-09-24 20:27:53 -0400

    Remove dead code

--- src/types.ts
+++ src/types.ts
@@ -3,5 +3,3 @@ import { NORMALIZED_X_RANGE_CHAR } from './constants';
 export type VersionComparator = '<' | '<=' | '=' | '>' | '>=' | '^' | '~';
 
 export type VersionNumberOrXRange = number | typeof NORMALIZED_X_RANGE_CHAR;
-
-export type VersionSpecifierType = 'clause' | 'range';
