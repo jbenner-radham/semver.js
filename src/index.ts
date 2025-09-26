e595eaaf4facff2a3f1d40cb33e2120631ad08f7
James Benner <hello@jamesbenner.com>
2025-09-26 14:51:50 -0400

    Add more exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
+export { default as Comparator } from './comparison/comparator';
 export { SPEC_VERSION } from './constants';
 export { default as is } from './is';
 export { default as parse } from './parse';
+export { default as Satisfier } from './satisfaction/satisfier';
 export { default as SemanticVersion } from './semantic-version';
+export { default as SpecifierValidator } from './specifier-validator';
